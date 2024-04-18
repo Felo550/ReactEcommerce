103e4192c0835c23aee4ccd86164c030dae70b78
Felipe Torres <filip-2@hotmail.com>
2024-04-18 16:49:36 -0500

    Se agregan primeros componentes, NAVBAR.jsx

---
+++ vite-project/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h3>ECommerce</h3>
+      <div>
+        <button>Curso Canto</button>
+        <button>Curso Bajo</button>
+        <button>Curso Bateria</button>
+      </div>
+    </nav>
+  )
+}
+export default NavBar
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     // Configura el esbuild loader para que reconozca los archivos .js como .jsx
+//     loader: {
+//       '.js': 'jsx'
+//     }
+//   }
+// });
